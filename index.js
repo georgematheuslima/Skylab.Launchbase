2118973cfe9e3ef64074240fd308ef96de4ecfe3
George Matheus <georgematheuslima@oulook.com>
2020-04-14 22:29:05 -0300

    usando valores boolean

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
-// Criar um programa que calcula a média
-// das notas dos alunos e envia
-// mensagem do cálculo da média.
 
 const AlunosdaTurmaA = [
-    {nome:"George", nota: 9.8},
-    {nome: "Matheus", nota: 5},
-    {nome: "fulano", nota: 3}
+    { nome: "George", nota: 9.8 },
+    { nome: "Matheus", nota: 5 },
+    { nome: "fulano", nota: 3 },
+    { nome: "Delsivan", nota: 10 }
 
 ]
-
 
 const AlunosdaTurmaB = [
-    {nome: "Cleito",nota: 10},
-    {nome: "Aristides",nota: 7},
-    {nome: "ciclano", nota: 5}
-
+    { nome: "Cleito", nota: 10 },
+    { nome: "Aristides", nota: 7 },
+    { nome: "ciclano", nota: 5 },
+    { nome: "beltrano", nota: 6 }
 ]
 
+function calculaMedia(alunos) {
+    let soma = 0
+    for (let i = 0; i < alunos.length; i++) {
+        soma = soma + alunos[i].nota
 
-function calculamedia(Alunos) {
-    return  (Alunos[0].nota + Alunos[1].nota + Alunos[2].nota) / 3
-    
+    }
+
+    const media = soma / alunos.length
+    return media
+
 }
- 
-const media1 = calculamedia(AlunosdaTurmaA)
-console.log(calculamedia(AlunosdaTurmaA))
-const media2 = calculamedia(AlunosdaTurmaB)
-console.log(calculamedia(AlunosdaTurmaB))
-// Se a média foi maior que 7, parabenizar a turma.
 
-function enviarmensagem(media, turma){
-    if (media > 7){ 
+function enviarmensagem(media, turma) {
+    if (media >= 7) {
         return console.log(`Parabéns ${turma}, sua média foi: ${media}`)
     }
-    else{
+    else {
         return console.log(`Média abaixo do esperado. A média da turma ${turma} foi: ${media}`)
     }
 }
 
+function marcarComoReprovado(aluno) {
+    aluno.reprovado = false;
+    if (aluno.nota < 7) {
+        aluno.reprovado = true;
+    }
+}
+
+function enviarMensagemReprovado(aluno) {
+    if (aluno.reprovado) {
+        console.log(`O aluno ${aluno.nome} está reprovado`)
+    }
+}
+
+function alunosReprovado(alunos) {
+    for (let aluno of alunos) {
+        marcarComoReprovado(aluno)
+        enviarMensagemReprovado(aluno)
+    }
+}
+
+const media1 = calculaMedia(AlunosdaTurmaA)
+const media2 = calculaMedia(AlunosdaTurmaB)
+
 enviarmensagem(media1, 'turmaA')
 enviarmensagem(media2, 'TurmaB')
+
+marcarComoReprovado(AlunosdaTurmaA)
+
+alunosReprovado(AlunosdaTurmaA)
+alunosReprovado(AlunosdaTurmaB)
