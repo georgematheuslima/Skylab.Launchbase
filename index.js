4b66be777a6beda9d190e470b96d52bb53860d37
George Matheus <geoorge.m@hotmail.com>
2020-04-15 01:14:06 -0300

    english

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-// Criar um programa que calcula a média
-// das notas dos alunos e envia
-// mensagem do cálculo da média.
 
-const aluno1 = 'George'
-const nota1 = 9.8
+const classA = [
+    { name: "George", grade: 9.8 },
+    { name: "Matheus", grade: 5 },
+    { name: "fulano", grade: 3 },
+    { name: "Delsivan", grade: 10 }
 
+]
 
-const aluno2 = "Matheus"
-const nota2 = 5
+const classB = [
+    { name: "Cleito", grade: 10 },
+    { name: "Aristides", grade: 7 },
+    { name: "ciclano", grade: 5 },
+    { name: "beltrano", grade: 6 }
+]
 
-const aluno3 = 'fulano'
-const nota3 = 3
+function calculateAverage(students) {
+    let sum = 0
+    for (let i = 0; i < students.length; i++) {
+        sum = sum + students[i].grade
 
+    }
 
-const media = (nota1 + nota2 + nota3) / 3 
+    const average = sum / students.length
+    return average
 
-console.log(`a media foi ${media}`)
-
-// Se a média foi maior que 7, parabenizar a turma.
-
-if (media > 7){ 
-    return console.log('Parabéns')
 }
-else{
-    return console.log('Média abaixo do esperado.')
+
+function sendMessage(average, Class) {
+    if (average >= 7) {
+        return console.log(`Congratulations ${Class}, your test grade is: ${average}`)
+    }
+    else {
+        return console.log(`${Class} your test grade was lower then spected. Average: ${average}`)
+    }
 }
+
+function markasflunked(student) {
+    student.flunked = false;
+    if (student.grade < 7) {
+        student.flunked = true;
+    }
+}
+
+function sendMessageflunked(student) {
+    if (student.flunked) {
+        console.log(`The student ${student.name} is flunked`)
+    }
+}
+
+function studentsflunked(students) {
+    for (let student of students) {
+        markasflunked(student)
+        sendMessageflunked(student)
+    }
+}
+
+const average1 = calculateAverage(classA)
+const average2 = calculateAverage(classB)
+
+sendMessage(average1, 'Class A')
+sendMessage(average2, 'Class B')
+
+markasflunked(classA)
+
+studentsflunked(classA)
+studentsflunked(classB)
