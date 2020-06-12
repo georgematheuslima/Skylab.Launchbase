53a4bf9c9b2fcc24ea94f17929cb8042d8426d75
George Matheus <60715730+georgematheuslima@users.noreply.github.com>
2020-06-12 13:29:48 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports = [
+    
+    {
+        id: "-VQPimwkstM",
+        title: "Menu de subcategorias com recursividade no JavaScript",
+        duration: "5 min",
+        price: "free"
+    },
+    {
+        id: "XomJBk4krnI",
+        title: "Idade na programação, Medo de entrevista, Realocação no Mercado e Trabalho Remoto",
+        duration: "10min",
+        price:""
+    }
+]
